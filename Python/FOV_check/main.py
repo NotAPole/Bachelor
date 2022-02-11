2b52d034c39e81a90cd1ae9dd2f2fb909072ce00
NotAPole <pfurnes@hotmail.com>
2022-02-11 11:51:59 +0100

    started FOV code

--- Python/FOV_check/main.py
+++ Python/FOV_check/main.py
@@ -9,16 +9,19 @@ the resulting 3D-plot show green and red points. green means seen, red means uns
 """
 import time
 
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class Camera:
-    def __init__(self, x, y, z, FOV, focus):
+    def __init__(self, x, y, z, FOV, focus, range):
         self.x = x
         self.y = y
         self.z = z
         self.FOV = FOV
         self.focus = focus
+        self.range = range
 
     def check_possible_obstructions(self, point, pallet_faces):
         posObs = []
@@ -45,10 +48,6 @@ class Camera:
         x = np.linspace(self.x, point[0], step)
         y = np.linspace(self.y, point[1], step)
         z = np.linspace(self.z, point[2], step)
-        #for i in range(len(x)):
-        #    x[i] = x[i].round(5)
-        #    y[i] = y[i].round(5)
-        #    z[i] = z[i].round(5)
 
         return x, y, z
 
@@ -58,7 +57,7 @@ class Camera:
         thresholdPoint = tP
         for j in range(step):
             for k, l in enumerate(equations):
-                planar_result = l[0]*(x[j]-l[1]) + l[2]*(y[j]-l[3]) + l[4]*(z[j]-l[5])
+                planar_result = l[0] * (x[j] - l[1]) + l[2] * (y[j] - l[3]) + l[4] * (z[j] - l[5])
                 crossingPlane = abs(planar_result) < thresholdCrossing
                 if crossingPlane:
                     corners_x = (np.min([surfaces[k][0][0], surfaces[k][1][0], surfaces[k][2][0], surfaces[k][3][0]]),
@@ -67,18 +66,61 @@ class Camera:
                                  np.max([surfaces[k][0][1], surfaces[k][1][1], surfaces[k][2][1], surfaces[k][3][1]]))
                     corners_z = (np.min([surfaces[k][0][2], surfaces[k][1][2], surfaces[k][2][2], surfaces[k][3][2]]),
                                  np.max([surfaces[k][0][2], surfaces[k][1][2], surfaces[k][2][2], surfaces[k][3][2]]))
-                    conObs = (corners_x[0] - 0.1) <= x[j] <= (corners_x[1] + thresholdPoint) and \
+                    conObs = (corners_x[0] - thresholdPoint) <= x[j] <= (corners_x[1] + thresholdPoint) and \
                              (corners_y[0] - thresholdPoint) <= y[j] <= (corners_y[1] + thresholdPoint) and \
                              (corners_z[0] - thresholdPoint) <= z[j] <= (corners_z[1] + thresholdPoint) and \
-                             not(point[0]-thresholdPoint <= x[j] <= point[0]+thresholdPoint and
-                                 point[1] - thresholdPoint <= y[j] <= point[1] + thresholdPoint and
-                                 point[2] - thresholdPoint <= z[j] <= point[2] + thresholdPoint)
+                             not (point[0] - thresholdPoint <= x[j] <= point[0] + thresholdPoint and
+                                  point[1] - thresholdPoint <= y[j] <= point[1] + thresholdPoint and
+                                  point[2] - thresholdPoint <= z[j] <= point[2] + thresholdPoint)
 
                     if conObs:
                         return True
 
         return False
 
+    def check_points_in_FOV(self, points):
+        possiblePoints = []
+        pointsOutsideFOV = []
+        unitFocusVector = self.get_focus_vector()
+
+        xyVectors = self.get_spread_vectors_xy(unitFocusVector, self.FOV[0])
+        #zVectors = self.get_spread_vectors_z(unitFocusVector, self.FOV[1])
+        for i in points:
+            dist = np.sqrt((i[0] - self.x) ** 2 + (i[1] - self.y)**2 + (i[2] - self.z) ** 2)
+            if dist <= self.range:
+                pass
+
+        return possiblePoints, pointsOutsideFOV
+
+    def get_focus_vector(self):
+        vector = numpy.array([self.focus[0]-self.x, self.focus[1]-self.y, self.focus[2]-self.z])
+
+        return vector / ((vector ** 2).sum() ** 0.5)
+
+    @staticmethod
+    def get_spread_vectors_xy(focusVector, spread):
+        spreadRad = spread*np.pi/180
+        print(focusVector, spread)
+        rotationalMatrix1 = [
+            [np.cos(spreadRad / 2), -np.sin(spreadRad / 2), 0],
+            [np.sin(spreadRad / 2), np.cos(spreadRad / 2), 0],
+            [0, 0, 1]
+        ]
+        rotationalMatrix2 = [
+            [np.cos(-spread / 2), -np.sin(-spread / 2), 0],
+            [np.sin(-spread / 2), np.cos(-spread / 2), 0],
+            [0, 0, 1]
+        ]
+
+        vectors = [np.matmul(rotationalMatrix1, focusVector), np.matmul(rotationalMatrix2, focusVector)]
+
+        return vectors
+
+    @staticmethod
+    def get_spread_vectors_z(focusVector, spread):
+        spreadRad = spread*np.pi/180
+        print(focusVector)
+        return vectors
 
 class Pallet:
     def __init__(self, faces):
@@ -97,7 +139,8 @@ class Pallet:
 
         return equations
 
-def show_plots(seenPoints, obstructedPoints, palletFaces, cameras):
+
+def show_plots(seenPoints, obstructedPoints, cameras):
     X_seen, Y_seen, Z_seen = [], [], []
     for j in seenPoints:
         X_seen.append(j[0])
@@ -131,8 +174,42 @@ def get_plane(point, normal):
 
 
 def main():
+    steps = 1000
+    thresholdSurface = 0.005
+    thresholdPoint = 0.02
     startTime = time.time()
-    with open("euro.txt") as palletFile:
+    palletSurfaceNum, remainingPoints = read_file("euro.txt")
+
+    cameras = []
+    cameras.append(Camera(40, 60, 50, (10, 10), (0, 0, 0), 150))
+    # cameras.append(Camera(40, 60, -50, (10, 10), (0, 0, 0)))
+    cameras.append(Camera(-20, -20, 7, (10, 10), (0, 0, 0), 150))
+    p1 = Pallet(palletSurfaceNum)
+
+    seenPoints = []
+    currentCamera = 1
+    for i in cameras:
+        remainingPoints, obstructedPoints = i.check_points_in_FOV(remainingPoints)
+        cameraProgress = 0
+        for j in remainingPoints:
+            pointObstructed = i.check_FOV(j, p1.faces, p1.equations, steps, thresholdSurface, thresholdPoint)
+            if pointObstructed:
+                obstructedPoints.append(j)
+            else:
+                seenPoints.append(j)
+            cameraProgress += 1
+            print("Camera " + str(currentCamera) + " " + str(
+                round(cameraProgress / len(remainingPoints) * 100, 2)) + "% done")
+        remainingPoints = obstructedPoints
+        currentCamera += 1
+
+    print("Total time used:", time.time() - startTime)
+
+    show_plots(seenPoints, obstructedPoints, cameras)
+
+
+def read_file(file):
+    with open(file) as palletFile:
         palletSurfaceText = palletFile.read()
         palletSurfaceText = palletSurfaceText.replace("[", "")
         palletSurfaceText = palletSurfaceText.replace("]", "")
@@ -153,39 +230,8 @@ def main():
         for j in remainingPoints:
             if remainingPoints.count(j) > 1:
                 remainingPoints.remove(j)
-
-    cameras = []
-    cameras.append(Camera(40, 60, 50, (10, 10), (0, 0, 0)))
-    #cameras.append(Camera(40, 60, -50, (10, 10), (0, 0, 0)))
-    cameras.append(Camera(-20, -20, 7, (10, 10), (0, 0, 0)))
-
-    p1 = Pallet(palletSurfaceNum)
-    seenPoints = []
-    currentCamera = 1
-    steps = 1000
-    thresholdSurface = 0.005
-    thresholdPoint = 0.02
-    for i in cameras:
-        obstructedPoints = []
-        cameraProgress = 0
-        for j in remainingPoints:
-            pointObstructed = i.check_FOV(j, p1.faces, p1.equations, steps, thresholdSurface, thresholdPoint)
-            if pointObstructed:
-                obstructedPoints.append(j)
-            else:
-                seenPoints.append(j)
-            cameraProgress += 1
-            print("Camera " + str(currentCamera) + " " + str(round(cameraProgress / len(remainingPoints) * 100, 2)) + "% done")
-        remainingPoints = obstructedPoints
-        currentCamera += 1
-
-    print("Total time used:", time.time() - startTime)
-    show_plots(seenPoints, obstructedPoints, p1.faces, cameras)
+    return palletSurfaceNum, remainingPoints
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
