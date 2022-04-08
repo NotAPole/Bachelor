c0706087e44392b6e666544591c0b133ba7ee500
mariushmelaas <73130882+mariushmelaas@users.noreply.github.com>
2022-04-08 13:09:44 +0200

    Added camera and rangefinder
    Added the script from the camera and rangefined into marius_simulation to start building Sort

---
+++ cam/controllers/camera/camera.py
@@ -0,0 +1,51 @@
+"""camera controller."""
+
+from controller import Robot, Camera, RangeFinder
+
+#lagger en klasse som brukes for alle robotene, navnet på child komponentene må vere likt    
+class platform (Robot):
+    timestep = 32
+    def __init__(self):
+        super(platform, self).__init__()
+        #setter opp kamera
+        self.camera = self.getDevice('camera')
+        self.camera.enable(4*self.timestep)
+        #setter opp rangefinderen
+        self.rangeFinder = self.getDevice('3d')
+        self.rangeFinder.enable(4*self.timestep)
+        #setter opp keyboard detection
+        self.keyboard.enable(self.timestep)
+        self.keyboard = self.getKeyboard()
+        #lar deg gjøre forskjellige ting med hver robot
+        self.ID = self.getName()
+        if self.ID == 'framework1':
+            print('robot1 identified')
+            self.filename = ['cam1_dist.txt','cam1_rgb.txt']
+        elif self.ID == 'framework2':
+            print('robot2 identified')
+            self.filename = ['cam2_dist.txt','cam2_rgb.txt']
+        
+    def run(self):
+        while True:
+            #2d array med alle dybder
+            self.depth = self.rangeFinder.getRangeImageArray()
+            #array med RGB info for siste bilde
+            #formen er array[x][y][n] hvor n = 0 er rød, 1 er grøn og 2 er blå
+            self.image = self.camera.getImageArray()
+            
+            k = self.keyboard.getKey()
+            
+            if k == ord('Q'):
+                file = open(self.filename[0], "w+")
+                content = str(self.depth)
+                file.write(content)
+                file.close()
+                file = open(self.filename[1], "w+")
+                content = str(self.image)
+                file.write(content)
+                file.close()
+            if self.step(self.timestep) == -1:
+                break
+controller = platform()
+controller.run()
+            
