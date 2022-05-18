146c5ac8942d1d192164d6062ef27d58ec07d951
Oddt94 <73301743+Oddt94@users.noreply.github.com>
2022-05-18 23:10:20 +0200

    Added final sim files

---
+++ zivid test/controllers/camera/camera.py
@@ -0,0 +1,75 @@
+"""camera controller."""
+
+from controller import Robot, Camera, RangeFinder
+import math
+import numpy as np
+
+#lagger en klasse som brukes for alle robotene, navnet på child komponentene må vere likt    
+class platform (Robot):
+    timestep = 32
+    def __init__(self):
+        super(platform, self).__init__()
+        #setter opp kamera
+        self.camera = self.getDevice('camera')
+        self.camera.enable(4*self.timestep)
+        #setter opp rangefinderen
+        self.rangeFinder = self.getDevice('3d')
+        self.rangeFinder.enable(4*self.timestep)
+        #setter opp keyboard detection
+        self.keyboard.enable(self.timestep)
+        self.keyboard = self.getKeyboard()
+        #lar deg gjøre forskjellige ting med hver robot
+        self.ID = self.getName()
+        if self.ID == 'framework1':
+            print('robot1 identified')
+            self.filename = ['cam1_dist.txt','cam1_rgb.txt']
+            image_width = self.rangeFinder.getWidth()
+            image_height = self.rangeFinder.getHeight()
+            focal_length = 0.5 * image_width * (1 / math.tan(0.5 * self.rangeFinder.getFov()))
+            k_matrix = np.array([
+                [focal_length, 0, image_width / 2],
+                [0, focal_length, image_height / 2],
+                [0, 0, 0]
+            ])
+            file = open('k_matrix.txt', "w+")
+            for row in k_matrix:
+                np.savetxt(file, row)
+            file.close()
+        elif self.ID == 'framework2':
+            print('robot2 identified')
+            self.filename = ['cam2_dist.txt','cam2_rgb.txt']
+        elif self.ID == 'framework3':
+            print('robot3 identified')
+            self.filename = ['cam3_dist.txt','cam3_rgb.txt']
+        
+    def run(self):
+        while True:
+            #2d array med alle dybder
+            self.depth = self.rangeFinder.getRangeImageArray()
+            #array med RGB info for siste bilde
+            #formen er array[x][y][n] hvor n = 0 er rød, 1 er grøn og 2 er blå
+            self.image = self.camera.getImageArray()
+            
+            k = self.keyboard.getKey()
+            
+            if k == ord('Q'):
+                file = open(self.filename[0], "w+")
+                #content = str(self.depth)
+                #file.write(content)
+                for row in self.depth:
+                    np.savetxt(file, row)
+                file.close()
+                file = open(self.filename[1], "w+")
+                #content = str(self.image)
+                #file.write(content)
+                for row in self.image:
+                    np.savetxt(file, row)
+                file.close()
+                
+                
+            if self.step(self.timestep) == -1:
+                break
+                
+controller = platform()
+controller.run()
+            
---
+++ zivid test/controllers/linear_axis_python_controller/linear_axis_python_controller.py
@@ -0,0 +1,99 @@
+"""linear axis controller."""
+from controller import Robot, Motor, Camera, RangeFinder
+from math import pi, sin
+
+class platform (Robot):
+    timestep = 32
+
+    def __init__(self):
+        super(platform, self).__init__()
+        
+        self.motorAngle = self.getDevice("motor")
+        self.motorAngle.setPosition(float('inf'))
+        self.motorAngle.setVelocity(0.0)
+
+        self.pSensor = self.getDevice("ps")
+        self.pSensor.enable(self.timestep)
+        
+        self.vSensor = self.getDevice("vs")
+        self.vSensor.enable(self.timestep)
+        
+        self.motor = self.getDevice("linear")
+        #setter opp kamera
+        self.camera = self.getDevice('camera1')
+        self.camera.enable(4*self.timestep)
+        #setter opp rangefinderen
+        self.rangeFinder = self.getDevice('3d1')
+        self.rangeFinder.enable(4*self.timestep)
+        #setter opp keyboard detection
+        self.keyboard.enable(self.timestep)
+        self.keyboard = self.getKeyboard()
+        #lar deg gjøre forskjellige ting med hver robot
+        self.ID = self.getName()
+        if self.ID == 'metal_pole':
+            print('The linear axis identified')
+            self.filename = ['cam1_dist.txt','cam1_rgb.txt']
+        
+    def run(self):
+        while True:
+            #2d array med alle dybder
+            self.depth = self.rangeFinder.getRangeImageArray()
+            #array med RGB info for siste bilde
+            #formen er array[x][y][n] hvor n = 0 er rød, 1 er grøn og 2 er blå
+            self.image = self.camera.getImageArray()
+            
+            k = self.keyboard.getKey()
+
+            
+            if k == ord('Q'):
+                file = open(self.filename[0], "w+")
+                content = str(self.depth)
+                file.write(content)
+                file.close()
+                file = open(self.filename[1], "w+")
+                content = str(self.image)
+                file.write(content)
+                file.close()
+            if self.step(self.timestep) == -1:
+                break
+    def camera_movement(self):
+
+  
+        speed=1
+        vertical_value = 0 
+        horizontal_value = 0
+        position = 0.0
+        constant = 1
+        while self.step(self.timestep) != -1:
+
+            n = self.keyboard.getKey()  
+            
+            # Verical controle
+            self.motor.setPosition(position)
+            vertical_value = self.vSensor.getValue()
+            print("Vertical position", vertical_value)
+            
+            # Angle controle 
+            self.motorAngle.setVelocity(speed)
+            horizontal_value = self.pSensor.getValue()
+            print("Horizontal angle", horizontal_value)   
+            
+            if n == ord('S'):
+                print("Going down")
+                position -= constant
+                speed -= 1
+
+         
+
+            if n == ord('W'):
+                print("Going up")
+                position += constant
+                speed += 1
+   
+            
+            pass   
+ 
+            
+controller = platform()
+controller.camera_movement()
+#controller.run()
